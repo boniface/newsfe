053ebffe1a09e4925f30942acd9c90e070297486
hashcode <boniface@kabaso.com>
2020-12-12 19:10:11 +0200

    Added Website Feature

---
+++ src/app/articles/state/store/articles-store.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {EntityState, EntityStore, StoreConfig} from '@datorama/akita';
+import {Article} from '../models/article.model';
+
+export interface ArticlesState extends EntityState<Article, string> {
+
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+@StoreConfig({
+  name: 'articles',
+  idKey: 'id',
+  cache: {
+    ttl: 3600000
+  }
+})
+export class ArticlesStore extends EntityStore<ArticlesState, Article> {
+  constructor() {
+    super();
+  }
+}
