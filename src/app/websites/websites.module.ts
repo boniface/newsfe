aa45dec08c7f3d2930554001785ca4bfdfd1d2ae
hashcode <boniface@kabaso.com>
2020-12-18 07:42:52 +0200

    message

--- src/app/websites/websites.module.ts
+++ src/app/websites/websites.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { WebsitesRoutingModule } from './websites-routing.module';
 import { WebsitesComponent } from './pages/containers/websites.component';
 import { WebsiteComponent } from './pages/components/website/website.component';
+import {MatSliderModule} from '@angular/material/slider';
 
 
 @NgModule({
   declarations: [WebsitesComponent, WebsiteComponent],
-  imports: [
-    CommonModule,
-    WebsitesRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        WebsitesRoutingModule,
+        MatSliderModule
+    ]
 })
 export class WebsitesModule { }
