7b519c3eb4532bb6bdb2394b2955de1e5708661a
hashcode <boniface@kabaso.com>
2020-12-18 20:35:23 +0200

    Added Website Feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './modules/general/home/home.component';
-import { NotFoundComponent } from './modules/general/not-found/not-found.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './modules/general/home/home.component';
+import {NotFoundComponent} from './modules/general/not-found/not-found.component';
+import {AppRoutingModule} from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatBadgeModule} from '@angular/material/badge';
 
 @NgModule({
   declarations: [
@@ -16,9 +19,13 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatSliderModule,
+    MatNativeDateModule,
+    MatBadgeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/websites/websites.module.ts
+++ src/app/websites/websites.module.ts
@@ -5,14 +5,20 @@ import { WebsitesRoutingModule } from './websites-routing.module';
 import { WebsitesComponent } from './pages/containers/websites.component';
 import { WebsiteComponent } from './pages/components/website/website.component';
 import {MatSliderModule} from '@angular/material/slider';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [WebsitesComponent, WebsiteComponent],
-    imports: [
-        CommonModule,
-        WebsitesRoutingModule,
-        MatSliderModule
-    ]
+  imports: [
+    CommonModule,
+    WebsitesRoutingModule,
+    MatSliderModule,
+    MatCardModule,
+    MatDividerModule,
+    MatIconModule
+  ]
 })
 export class WebsitesModule { }
