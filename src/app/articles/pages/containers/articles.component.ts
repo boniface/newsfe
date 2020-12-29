26a4656c75594b8cd65e90e1f74c7a2c0bcc13ca
victor <mwanzav1@gmail.com>
2020-12-29 13:25:01 +0200

    collections done

--- src/app/articles/pages/containers/articles.component.ts
+++ src/app/articles/pages/containers/articles.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AppDates} from '../../../shared/util/AppDates';
 
 @Component({
   selector: 'app-articles',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./articles.component.css']
 })
 export class ArticlesComponent implements OnInit {
-
+testdate;
   constructor() { }
 
   ngOnInit(): void {
+    this.testdate = AppDates.lastMonth();
   }
 
 }
