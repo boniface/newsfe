251676862bb1321151537b21af9f53272c75de5a
hashcode <boniface@kabaso.com>
2021-01-01 09:40:13 +0200

    Added Website Feature

--- src/app/articles/pages/containers/articles.component.ts
+++ src/app/articles/pages/containers/articles.component.ts
@@ -1,19 +1,49 @@
 import {Component, OnInit} from '@angular/core';
-import {AppDates} from '../../../shared/util/AppDates';
+import {BaseComponent} from '../../../shared/BaseComponent';
+import {Article} from '../../state/models/article.model';
+import {CanonicalService} from '../../../shared/util/canonical.service';
+import {Meta, Title} from '@angular/platform-browser';
+import {Router} from '@angular/router';
+import {ArticlesQueries} from '../../state/queries/articles-queries';
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.css']
 })
-export class ArticlesComponent implements OnInit {
-  testdate;
+export class ArticlesComponent extends BaseComponent implements OnInit {
+  data: Date = new Date();
+  title = 'Zambia Hash: The Single Source of All the News Headlines in Zambia';
+  todayArticles: Article[];
+  yesterdayArticles: Article[];
+  thisWeekArticles: Article[];
+  lastWeekArticles: Article[];
+  thisMonthArticles: Article[];
+  lastMonthArticles: Article[];
 
-  constructor() {
+  constructor(
+    private canonicalService: CanonicalService,
+    private titleService: Title,
+    private metaTagService: Meta,
+    private router: Router,
+    private articleQueries: ArticlesQueries,
+  ) {
+    super();
   }
 
   ngOnInit(): void {
-    this.testdate = AppDates.lastWeek();
+    this.canonicalService.setCanonicalURL();
+    this.titleService.setTitle(this.title);
+    this.metaTagService.updateTag(
+      {name: 'description', content: 'The Single Source of all online news Headlines from Zambia'}
+    );
+    this.articleQueries.getArticles()
+      .pipe(takeUntil(this.destroyed))
+      .subscribe(aricles => {
+        this.todayArticles = aricles;
+      });
+
   }
 
 }
