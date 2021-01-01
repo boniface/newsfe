9dc15295a8c5f7861ab3c7b90a4f3f4c6b107d82
hashcode <boniface@kabaso.com>
2021-01-01 14:58:33 +0200

    Added Website Feature

--- src/app/articles/article.module.ts
+++ src/app/articles/article.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { ArticleRoutingModule } from './article-routing.module';
 import { ArticlesComponent } from './pages/containers/articles.component';
 import { ArticleComponent } from './pages/components/article/article.component';
+import { DateAgoPipe } from './pipe/date-ago.pipe';
+import { ExcerptPipe } from './pipe/excerpt.pipe';
 
 
 
 @NgModule({
-  declarations: [ArticlesComponent, ArticleComponent],
+  declarations: [ArticlesComponent, ArticleComponent, DateAgoPipe, ExcerptPipe],
     imports: [
         CommonModule,
         ArticleRoutingModule,
